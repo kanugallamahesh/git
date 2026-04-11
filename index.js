45892a80fca25fdf5af84d4a2df7d1f5b3caf9a9
Mahesh <kanugallamahesh@gmail.com>
2026-04-11 09:28:42 +0530

    babu

---
+++ index.js
@@ -0,0 +1,40 @@
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+// let c = 32;
+//  c = 64;
+// console.log(c);
+
+// const b = 54;
+// console.log(b);
+
+
+// for(let i=0;i<=4;i++)
+// {
+//     for(let j=0;j<=4;j++)
+//     {
+//         console.log("*");
+//     }
+//     console.log(" "); 
+// }
+
+
+// let num1=10;
+// let num2="10";
+// if(num1==num2)
+// {
+//     console.log(-1);
+// }
+// else{
+//     console.log(1);
+// }
+
+// Arrays
+
+let userdata=[10,"hello",false,3.4567,null];
+for(let index=0;index<userdata.length;index++){
+    console.log( typeof userdata[index]);
+}
+
+
