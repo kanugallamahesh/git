1f31ea06164767150e1233bd725d8a7d100fc35e
Mahesh <kanugallamahesh@gmail.com>
2026-04-13 09:15:15 +0530

    objects are created

--- index.js
+++ index.js
@@ -1,40 +1,90 @@
-// var a = 10;
-// var a = 20;
-// console.log(a);
+// // var a = 10;
+// // var a = 20;
+// // console.log(a);
 
-// let c = 32;
-//  c = 64;
-// console.log(c);
+// // let c = 32;
+// //  c = 64;
+// // console.log(c);
 
-// const b = 54;
-// console.log(b);
+// // const b = 54;
+// // console.log(b);
 
 
-// for(let i=0;i<=4;i++)
-// {
-//     for(let j=0;j<=4;j++)
-//     {
-//         console.log("*");
-//     }
-//     console.log(" "); 
+// // for(let i=0;i<=4;i++)
+// // {
+// //     for(let j=0;j<=4;j++)
+// //     {
+// //         console.log("*");
+// //     }
+// //     console.log(" "); 
+// // }
+
+
+// // let num1=10;
+// // let num2="10";
+// // if(num1==num2)
+// // {
+// //     console.log(-1);
+// // }
+// // else{
+// //     console.log(1);
+// // }
+
+// // Arrays
+
+// let userdata=[10,"hello",false,3.4567,null];
+// for(let index=0;index<userdata.length;index++){
+//     console.log( typeof userdata[index]);
+    
 // }
+// console.log(userdata);
+// //userdata.push(20)
+// //userdata.pop(2938)
+// userdata.shift()
+// userdata.unshift("chotu")
+// console.log(userdata); 
 
 
-// let num1=10;
-// let num2="10";
-// if(num1==num2)
-// {
-//     console.log(-1);
+// const obj = {
+//     id:1,
+//     name:"guest",
+//     mydata:[10, "hello"," hiii,hii"]
 // }
-// else{
-//     console.log(1);
-// }
+// console.log(obj.id);
 
-// Arrays
+// obj.loc="hyd"
+// console.log(obj.mydata);
 
-let userdata=[10,"hello",false,3.4567,null];
-for(let index=0;index<userdata.length;index++){
-    console.log( typeof userdata[index]);
+
+const user= [
+    {
+    id:1,
+    name:"hii",
+    loc:"hyd"
+},
+{
+
+    id:2,
+    name:"bye",
+    loc :"knl"
 }
+];
+
+for (const element of user) {
+    for (const key in element) {
+        if (!Object.hasOwn(element, key)) continue;
+        
+        //const element = object[key];
+        console.log(element[key])
+        
+    }
+}
+//console.log(user[0].name)
+
+
+
+
+
+
 
 
